820b46fd87c8a6671bb4afea251a9cdf46ed8688
Elizabet <82437287+Elizabeth552@users.noreply.github.com>
2021-05-02 18:54:18 +0300

    Add files via upload

---
+++ Practice on Threads and Files - Warehouse task/src/main/Supplier.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Supplier extends Thread {
+
+    private Warehouse warehouse;
+
+    public Supplier(Warehouse warehouse) {
+        super("Supplier");
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            warehouse.sipi();
+        }
+    }
+}
